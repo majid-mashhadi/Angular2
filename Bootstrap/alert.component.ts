fdc5621c1c0ee8d3aadc9f93394dac07220f28cd
majid-mashhadi <majid1347@yahoo.com>
2017-09-18 13:32:29 -0700

    Add files via upload

---
+++ Bootstrap/alert.component.ts
@@ -0,0 +1,26 @@
+﻿import { Component, Input, OnInit, OnChanges, Output, EventEmitter, ChangeDetectionStrategy,SimpleChange} from '@angular/core'
+import { AlertConfig, AlertType } from './alert.config'
+
+@Component({
+    selector: 'alert',
+    templateUrl: './alert.component.html'
+})
+
+export class AlertComponent implements OnChanges {
+
+    @Input() config: AlertConfig;
+    @Output() close: EventEmitter<string> = new EventEmitter<string>();
+
+    ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
+        for (let propName in changes) {
+            let changedProp = changes[propName];
+            this.config = changedProp.currentValue;
+        }
+    }
+
+    hide(): void {
+        this.config.show = false;
+        this.close.emit('');
+    }
+}
+
---
+++ Bootstrap/alert.config.ts
@@ -0,0 +1,24 @@
+﻿import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AlertConfig {
+    dismiss: boolean;
+    type: string;
+    show: boolean;
+    constructor(dismiss: boolean, type: AlertType, show: boolean) {
+        this.dismiss = dismiss;
+        this.show = show;
+        this.type = AlertType[type];
+    }
+}
+
+export enum AlertType {
+    danger,
+    success,
+    error,
+    info,
+    warning
+}
+
+export class Alert {
+}
